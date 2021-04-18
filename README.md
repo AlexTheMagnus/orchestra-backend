164f23d28e6a5d524d9e681757b6eaea92c20165
AlexTheMagnus <i72lulem@uco.es>
2021-04-18 17:57:50 +0200

    add all tables

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 ## Usage
 
 1. Install all the dependencies running `sudo apt install python3.8 python3.8-dev virtualenv build-essential mysql-server mysql-client libmysqlclient-dev libsqlclient-dev libssl-dev -y`
-1. Create a Python 3.8 Virtual environment
+1. Create a Python 3.8 Virtual environment (virtualenv -p python3.8 venv)
 1. Activate it running `source venv/bin/activate`
 1. Inside the environment run `pip install -r requirements`
 1. Run `docker-compose up -d`
--- init_dbs.py
+++ init_dbs.py
@@ -20,49 +20,62 @@ def remove_existing_tables(engine):
 
 def create_user_table():
     db.Table('user', metadata,
-             db.Column('user_id', db.String(36),
-                       nullable=False, primary_key=True),
-             db.Column('username', db.String(255), nullable=False),
-             db.Column('avatar', db.String(255), nullable=False)
-             )
+							db.Column('user_id', db.String(36),
+												nullable=False, primary_key=True),
+							db.Column('username', db.String(255), nullable=False),
+							db.Column('avatar', db.String(255), nullable=False)
+						)
 
 
 def create_follow_table():
     db.Table('follow', metadata,
-             db.Column('follower', db.String(36),
-                       nullable=False, db.ForeignKey("user.user_id"), primary_key=True),
-             db.Column('followed', db.String(36),
-                       nullable=False, db.ForeignKey("user.user_id"), primary_key=True)
-             )
+							db.Column('follower', db.String(36),
+													db.ForeignKey("user.user_id"), nullable=False, primary_key=True),
+							db.Column('followed', db.String(36),
+													db.ForeignKey("user.user_id"), nullable=False, primary_key=True)
+						)
 
 
 def create_soundtrack_table():
     db.Table('soundtrack', metadata,
-             db.Column('soundtrack_id', db.String(36),
-                       nullable=False, primary_key=True),
-             db.Column('soundtrack_title', db.String(255), nullable=False),
-             db.Column('book_isbn', db.String(255), nullable=False),
-             db.Column('author', db.String(255), db.ForeignKey(
-                 "user.user_id"), nullable=False),
-             )
+							db.Column('soundtrack_id', db.String(36),
+												nullable=False, primary_key=True),
+							db.Column('soundtrack_title', db.String(255), nullable=False),
+							db.Column('isbn', db.String(255), nullable=False),
+							db.Column('author', db.String(36), db.ForeignKey(
+									"user.user_id"), nullable=False)
+						)
 
 
-def create_favorite_soundtrack_table():
-    db.Table('favorite_soundtrack', metadata,
-             db.Column('user_id', db.String(36),
-                       nullable=False, primary_key=True),
-             db.Column('soundtrack_id', db.Integer(),
-                       nullable=False, primary_key=True)
-             )
+def create_favorite_table():
+    db.Table('favorite', metadata,
+							db.Column('user_id', db.String(36), db.ForeignKey("user.user_id"), nullable=False, primary_key=True),
+							db.Column('soundtrack_id', db.String(36), db.ForeignKey("soundtrack.soundtrack_id"), 
+												nullable=False, primary_key=True)
+						)
 
+def create_like_table():
+    db.Table('like', metadata,
+             	db.Column('user_id', db.String(36), db.ForeignKey("user.user_id"), nullable=False, primary_key=True),
+             	db.Column('soundtrack_id', db.String(36), db.ForeignKey("soundtrack.soundtrack_id"), nullable=False, primary_key=True)
+						)
+
+def create_chapter_table():
+    db.Table('chapter', metadata,
+							db.Column('chapter_id', db.String(36), nullable=False, primary_key=True),
+							db.Column('soundtrack_id', db.String(36), db.ForeignKey("soundtrack.soundtrack_id"), nullable=False),
+							db.Column('theme_id', db.String(36), nullable=False),
+							db.Column('chapter_title', db.String(255), nullable=True),
+							db.Column('number', db.Integer(), nullable=False)
+            )
 
 if __name__ == "__main__":
     for engine in engines.values():
         remove_existing_tables(engine)
 
     create_user_table()
-    create_watched_movies_table()
-    create_following_movies_table()
+    create_follow_table()
+    create_soundtrack_table()
 
     for engine in engines.values():
         metadata.create_all(engine)
