e430cf01d4a5bd6a87cf3485c6d1a9ce8f5136ed
AlexTheMagnus <i72lulem@uco.es>
2021-05-01 18:44:56 +0200

    add username class

---
+++ test/user/domain/test_username.py
@@ -0,0 +1,14 @@
+import pytest
+from faker import Faker
+
+from src.user.domain.username import Username
+
+fake = Faker()
+
+
+class TestUsername():
+
+    def test_from_string_constructor(self):
+        str_username = fake.pystr()
+        username = Username.from_string(str_username)
+        assert username.value == str_username
