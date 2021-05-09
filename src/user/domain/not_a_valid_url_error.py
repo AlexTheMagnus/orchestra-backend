da933f9ae1d7ecdc40170c903f7e9ec7eb869456
AlexTheMagnus <i72lulem@uco.es>
2021-05-09 18:25:42 +0200

    add module struture

--- src/user/domain/not_a_valid_url_error.py
+++
@@ -1,7 +0,0 @@
-class NotAValidUrlError(Exception):
-    def __init__(self, user_avatar_url: str):
-        self.message = 'User avatar: {0} is not a valid url.'.format(
-            user_avatar_url)
-
-    def __str__(self):
-        return self.message
