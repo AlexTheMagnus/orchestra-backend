694418d27e3b2955cc551ad83943e734940cb3be
AlexTheMagnus <i72lulem@uco.es>
2021-05-16 19:52:03 +0200

    wip: add test_isbn

---
+++ test/soundtrack/domain/test_isbn.py
@@ -0,0 +1,18 @@
+import pytest
+# import pysisbn
+
+from src.soundtrack.domain.isbn_13 import Isbn13
+from src.soundtrack.domain.not_a_valid_isbn_13_error import NotAValidIsbn13Error
+
+
+class TestIsbn13():
+
+    def test_from_string_constructor(self):
+        str_isbn_13 = str(uuid.uuid4())
+        isbn_13 = Isbn13.from_string(str_isbn_13)
+        assert isbn_13.value == str_isbn_13
+
+    def test_building_isbn_13_with_a_non_valid_isbn_13_throws_an_error(self):
+        str_isbn_13 = str(uuid.uuid1())
+        with pytest.raises(NotAValidIsbn13Error):
+            isbn_13 = SoundtrackId.from_string(str_isbn_13)
