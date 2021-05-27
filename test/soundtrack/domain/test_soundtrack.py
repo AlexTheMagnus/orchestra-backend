2046e148a2b3af192cd0b71a0a770a77663e303b
AlexTheMagnus <i72lulem@uco.es>
2021-05-27 17:53:26 +0200

    finished soundtrack entity

--- test/soundtrack/domain/test_soundtrack.py
+++ test/soundtrack/domain/test_soundtrack.py
@@ -1,4 +1,5 @@
 import pytest
+import uuid
 from faker import Faker
 
 from src.soundtrack.domain.soundtrack_id import SoundtrackId
@@ -14,14 +15,19 @@ fake = Faker()
 class TestSoundtrack():
 
     def test_soundtrack_constructor(self):
-        soundtrack_id: SoundtrackId = SoundtrackId.from_string(fake.pystr())
+        soundtrack_id: SoundtrackId = SoundtrackId.from_string(
+            str(uuid.uuid4()))
         book: Isbn13 = Isbn13.from_string("978-2-1550-9533-9")
         author: UserId = UserId.from_string(fake.pystr())
-        chapters: list[Chapter] = []
+        chapter1: Chapter = Chapter(
+            str(uuid.uuid4()), 1, "https: // open.spotify.com/track/" + fake.pystr(), "")
+        chapter2: Chapter = Chapter(
+            str(uuid.uuid4()), 2, "https: // open.spotify.com/track/" + fake.pystr(), "")
+        chapters: list[Chapter] = [chapter1, chapter2]
 
         soundtrack = Soundtrack(soundtrack_id, book, author, chapters)
 
         assert soundtrack.soundtrack_id.value == soundtrack_id.value
         assert soundtrack.book.value == book.value
         assert soundtrack.author.value == author.value
-        assert soundtrack.chapters.value == chapters.value
+        assert soundtrack.chapters == chapters
