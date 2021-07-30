fa3ff0e7c269f3a5d6d173119d9f183cca85d487
AlexTheMagnus <i72lulem@uco.es>
2021-07-30 21:35:29 +0200

    add: get user soundtracks use case

---
+++ tests/soundtrack/application/test_get_user_soundtracks.py
@@ -0,0 +1,31 @@
+import pytest
+import uuid
+from faker import Faker
+from typing import List
+
+from ..infrastructure.soundtrack_in_memory_repository import SoundtrackInMemoryRepository
+from src.soundtrack.domain.user_id import UserId
+from src.soundtrack.domain.soundtrack import Soundtrack
+from ..builder.soundtrack_builder import SoundtrackBuilder
+from src.soundtrack.application.get_user_soundtracks import GetUserSoundtracks
+
+fake = Faker()
+
+soundtrack_repository = SoundtrackInMemoryRepository()
+use_case: GetUserSoundtracks = GetUserSoundtracks(soundtrack_repository)
+author: UserId = UserId.from_string(fake.pystr())
+author_without_soundtracks: UserId = UserId.from_string(fake.pystr())
+
+for x in range(4):
+    soundtrack: Soundtrack = SoundtrackBuilder().with_author(author).build()
+    soundtrack_repository.save(soundtrack)
+
+
+class TestGetUserSoundtracks():
+    def test_user_soundtracks_are_getted(self):
+        found_soundtracks: List[Soundtrack] = use_case.run(author)
+        assert len(found_soundtracks) == 4
+
+    def test_user_without_soundtracks_return_no_soundtracks(self):
+        found_soundtracks: List[Soundtrack] = use_case.run(author_without_soundtracks)
+        assert len(found_soundtracks) == 0
--- tests/soundtrack/infrastructure/soundtrack_in_memory_repository.py
+++ tests/soundtrack/infrastructure/soundtrack_in_memory_repository.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 from src.soundtrack.domain.soundtrack_repository import SoundtrackRepository
 from src.soundtrack.domain.soundtrack import Soundtrack
 from src.soundtrack.domain.soundtrack_id import SoundtrackId
-
+from src.soundtrack.domain.user_id import UserId
 
 class SoundtrackInMemoryRepository(SoundtrackRepository):
     def __init__(self):
@@ -13,8 +13,15 @@ class SoundtrackInMemoryRepository(SoundtrackRepository):
         self.__soundtracks.append(soundtrack)
 
     def find(self, soundtrack_id: SoundtrackId) -> Optional[Soundtrack]:
-
         for soundtrack in self.__soundtracks:
             if soundtrack.soundtrack_id.value == soundtrack_id.value:
                 return soundtrack
         return None
+
+    def find_by_author(self, author: UserId):
+        found_soundtracks: List[Soundtrack] = []
+
+        for soundtrack in self.__soundtracks:
+            if soundtrack.author.value == author.value:
+                found_soundtracks.append(soundtrack)
+        return found_soundtracks
